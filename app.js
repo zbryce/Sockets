b5cd406794894a06e38359fbb1fb091d7be89da9
Zach Bryce <zachbryce11@gmail.com>
2020-08-16 07:14:14 -0500

    used backup

--- app.js
+++ app.js
@@ -5,117 +5,116 @@ const http = require('http')
 const socketio = require('socket.io')
 const server = http.createServer(app)
 const io = socketio(server)
-
+ 
 app.use(express.static(path.join(__dirname, 'public')))
-
+ 
 // app.get('/user', (req, res) => {
 //     res.sendFile(path.join(__dirname, ))
 // })
 const getVisitors = () => {
-    // let clients = io.sockets.clients.connected;
-    // let sockets = Object.values(clients)
-    // let users = sockets.map(s => s.user)
-    // return users
-    const users = io.sockets.clients()
-   
+   // let clients = io.sockets.clients.connected;
+   // let sockets = Object.values(clients)
+   // let users = sockets.map(s => s.user)
+   // return users
+   const users = io.sockets.clients()
+ 
 }
 const emitVisitors = () => {
-    io.emit('users', getVisitors())
+   io.emit('users', getVisitors())
 }
 const serveClients = (keys) => {
-    keys.forEach(key => {
-        console.log('key ', key)
-    });
+   keys.forEach(key => {
+       console.log('key ', key)
+   });
 }
 const connectedUsers = {
-
 }
 io.on('connection', (socket) => {
-    connectedUsers[socket.id] = socket
+   connectedUsers[socket.id] = socket
+ 
+       const admin = io.of('/admin')
+       admin.on('connection', (socket) => {
+           connectedUsers[socket.id] = socket
+         
+           // serveClients(clients)
+           socket.on('login', (obj) => {
+               console.log('api obj', obj)
+               socket.emit('admin-log', obj)
+               socket.broadcast.emit('new user', `${obj.username} ${socket.id} has joined the chat`)
+               socket.emit('client', [obj.username, 'red'])
+               socket.broadcast.emit('other client', [obj.username, 'red'])
+              
+               socket.emit('adminMember', [obj.username, socket.id])
+               socket.broadcast.emit('adminMember', [obj.username, socket.id])
+           })
+           socket.on('namespace', (username) => {
+              
+               admin.emit('userPush', ([`${username} pushed the admin namespace button!`, 'red']))
+           })
+           socket.on('get-id', (Private) => {
+               const { id, payload, username} = Private
+               const user = connectedUsers[id]
+               console.log('admin user ', user.id)
+               const socket = user
+               const line = `# ${id} ${username} says: ${payload}`
+               socket.volatile.emit('private', line)
+           })
+           socket.on('adminButton', (username) => {
+               admin.emit('adminClick', (`Admin ${username} sent a notice to everyone.`))
+               io.emit('adminClick', (`Admin ${username} sent a notice to everyone.`))
+           })
+           socket.on('message', (message => {
+            socket.volatile.emit('myMessage', message)
+            socket.broadcast.volatile.emit('theirMessage', message)
+            // socket.broadcast.emit('thereMessage', message)
 
-        const admin = io.of('/admin')
-        admin.on('connection', (socket) => {
-            connectedUsers[socket.id] = socket
-           
-            // serveClients(clients)
-            socket.on('login', (obj) => {
-                console.log('api obj', obj)
-                socket.emit('admin-log', obj)
-                socket.broadcast.emit('new user', `${obj.username} ${socket.id} has joined the chat`)
-                socket.emit('client', [obj.username, 'red'])
-                socket.broadcast.emit('other client', [obj.username, 'red'])
-                
-                socket.emit('adminMember', [obj.username, socket.id])
-                socket.broadcast.emit('adminMember', [obj.username, socket.id])
-            })
-            socket.on('namespace', (username) => {
-                
-                admin.emit('userPush', ([`${username} pushed the admin namespace button!`, 'red']))
-            })
-            socket.on('get-id', (Private) => {
-                const { id, payload, username} = Private
-                const user = connectedUsers[id]
-                console.log('admin user ', user.id)
-                const socket = user
-                const line = `# ${id} ${username} says: ${payload}`
-                socket.volatile.emit('private', line)
-            })
-            socket.on('adminButton', (username) => {
-                admin.emit('adminClick', (`Admin ${username} sent a notice to everyone.`))
-                io.emit('adminClick', (`Admin ${username} sent a notice to everyone.`))
-            })
-            socket.on('message', (message => {
-                socket.volatile.emit('myMessage', message)
-                socket.broadcast.volatile.emit('theirMessage', message)
-                // socket.broadcast.emit('thereMessage', message)
-
-            }))
-            socket.on('disconnect', () => {
-                console.log('admin left')
-            })
-        })
-        // socket.on('get-id', (idMessage) => {
-        //     console.log('user private')
-            
-        //     const {id, payload, username } = idMessage
-        //     console.log('cu id ', connectedUsers[id])
-        //     const user = connectedUsers[id]
-        //     console.log('user user ', user)
-        //     // const newMessage = `${user.id} ${username}: ${payload}`
-        //     // connectedUsers[id].emit('private', newMessage)
-           
-        // })
-        socket.on('login', (obj) => {
-            const room = obj.room
-            const username = obj.username
-            socket.join(room)
-            socket.emit('welcome', `Welcome to the ${room} room!`)
-            socket.to(room).emit('new user', `${username} ${socket.id} has joined the channel`)
-        })
-        socket.on('get-id', (Private) => {
-            const { id, payload, username} = Private
-            const user = connectedUsers[id]
-            const socket = user
-            const line = `# ${id} ${username} says: ${payload}`
-            socket.emit('private', line)
-        })
-        socket.on('message', (message, room) => {
-            console.log('user message')
-            socket.emit('myMessage', message)
-            socket.to(room).emit('theirMessage', message)
-        })
-        socket.on('namespace', (username) => {
-            console.log('namespace', username)
-            io.emit('userPush', [`${username} pushed the user namespace button!`, 'blue'])
-        })
+        }))
         socket.on('disconnect', () => {
-            console.log('disconnected')
+            console.log('admin left')
         })
+    })
+    // socket.on('get-id', (idMessage) => {
+    //     console.log('user private')
+       
+    //     const {id, payload, username } = idMessage
+    //     console.log('cu id ', connectedUsers[id])
+    //     const user = connectedUsers[id]
+    //     console.log('user user ', user)
+    //     // const newMessage = `${user.id} ${username}: ${payload}`
+    //     // connectedUsers[id].emit('private', newMessage)
+      
+    // })
+    socket.on('login', (obj) => {
+        const room = obj.room
+        const username = obj.username
+        socket.join(room)
+        socket.emit('welcome', `Welcome to the ${room} room!`)
+        socket.to(room).emit('new user', `${username} ${socket.id} has joined the channel`)
+    })
+    socket.on('get-id', (Private) => {
+        const { id, payload, username} = Private
+        const user = connectedUsers[id]
+        const socket = user
+        const line = `# ${id} ${username} says: ${payload}`
+        socket.emit('private', line)
+    })
+    socket.on('message', (message, room) => {
+        console.log('user message')
+        socket.emit('myMessage', message)
+        socket.to(room).emit('theirMessage', message)
+    })
+    socket.on('namespace', (username) => {
+        console.log('namespace', username)
+        io.emit('userPush', [`${username} pushed the user namespace button!`, 'blue'])
+    })
+    socket.on('disconnect', () => {
+        console.log('disconnected')
+    })
 })
 
 // adminnsp.on('connection', (socket) => {
 //     socket.on('conn')
 // })
 server.listen('1919', () => {
-    console.log('listening on 1919')
+console.log('listening on 1919')
 })
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,103 +1,104 @@
 let socket = io()
-
-
-
+ 
+ 
+ 
 const { username, room, admin } = Qs.parse(location.search, {
-    ignoreQueryPrefix: true
+   ignoreQueryPrefix: true
 });
-
-
+ 
+ 
 if(admin === 'milo'){
-    socket = io('/admin')
+   socket = io('/admin')
 }
 // console.log('socket is ', socket)
-
+ 
 console.log('username is', username)
 socket.emit('login', {username, room, admin})
-
-
+ 
+ 
 socket.once('admin-log', (message) => {
-    console.log('in admin log')
-    const sidebar = document.getElementById('sidebar-div')
-    sidebar.style.background = 'maroon'
-    const status = document.getElementById('status')
-    status.innerHTML = 'ADMIN'
-    const item = document.createElement('li')
-    item.classList.add('neutral')
-    item.innerHTML = `Welcome to the chat, ${message.username}`
-    list.appendChild(item)
+   console.log('in admin log')
+   const sidebar = document.getElementById('sidebar-div')
+   sidebar.style.background = 'maroon'
+   const status = document.getElementById('status')
+   status.innerHTML = 'ADMIN'
+   const item = document.createElement('li')
+   item.classList.add('neutral')
+   item.innerHTML = `Welcome to the chat, ${message.username}`
+   list.appendChild(item)
 })
 socket.on('myMessage', (message) => {
-    console.log('admin message ', message)
-    const myMessage = document.createElement('p')
-    myMessage.classList.add('myMessage')
-    myMessage.innerHTML = message
-    list.appendChild(myMessage)
+   console.log('admin message ', message)
+   const myMessage = document.createElement('p')
+   myMessage.classList.add('myMessage')
+   myMessage.innerHTML = message
+   list.appendChild(myMessage)
 })
 socket.on('theirMessage', (message) => {
-    console.log('their message ', message)
-    const there = document.createElement('p')
-    there.classList.add('theirMessage')
-    there.innerHTML = message
-                                                                                        
-    list.appendChild(there)
+   console.log('their message ', message)
+   const there = document.createElement('p')
+    
+   there.classList.add('theirMessage')
+   there.innerHTML = message
+                                                                                      
+   list.appendChild(there)
 })
 socket.on('welcome', (message) => {
-    console.log('in welcome', message)
-    const welcome = document.createElement('li')
-    welcome.classList.add('neutral')
-    welcome.innerHTML = message
-    list.appendChild(welcome)
+   console.log('in welcome', message)
+   const welcome = document.createElement('li')
+   welcome.classList.add('neutral')
+   welcome.innerHTML = message
+   list.appendChild(welcome)
 })
 socket.once('new user', (message) => {
-    const hello = document.createElement('li')
-    hello.classList.add('neutral')
-    hello.innerHTML = message
-    list.appendChild(hello)
+   const hello = document.createElement('li')
+   hello.classList.add('neutral')
+   hello.innerHTML = message
+   list.appendChild(hello)
 })
 //my message
 socket.on('message', (message) => {
-    console.log('in message ', message)
-    const item = document.createElement('li')
-    item.classList.add('message')
-    item.innerHTML = message
-    const list = document.querySelector('#list')
-    list.appendChild(item)
+   console.log('in message ', message)
+   const item = document.createElement('li')
+   item.classList.add('message')
+   item.innerHTML = message
+   const list = document.querySelector('#list')
+   list.appendChild(item)
 })
-
+ 
 socket.on('admin-message', (message) => {
-    const item = document.createElement('li')
-    item.classList.add('message')
-    item.innerHTML = message
-    list.appendChild(item)
+   const item = document.createElement('li')
+   item.classList.add('message')
+   item.innerHTML = message
+   list.appendChild(item)
 })
 socket.on('userPush', (messageColor) => {
-    const [message, color] = messageColor
-    const header = document.getElementById('notice')
-    header.innerHTML = message
-    header.classList.add(color)
+   const [message, color] = messageColor
+   const header = document.getElementById('notice')
+   header.innerHTML = message
+   header.classList.add(color)
 })
 socket.once('adminClick', (message) => {
-    const adminNotice = document.createElement('h2')
-    adminNotice.classList.add('adminMessage')
-    adminNotice.innerHTML = message
-    clients.appendChild(adminNotice)
+   const adminNotice = document.createElement('h2')
+   adminNotice.classList.add('adminMessage')
+   adminNotice.innerHTML = message
+   clients.appendChild(adminNotice)
 })
 socket.on('private', (message) => {
-    console.log('private msg ', message)
-    const privateMsg = document.createElement('h3')
-    privateMsg.classList.add('private')
-    privateMsg.innerHTML = message
-    clients.appendChild(privateMsg)
+   console.log('private msg ', message)
+   const privateMsg = document.createElement('h3')
+   privateMsg.classList.add('private')
+   privateMsg.innerHTML = message
+   clients.appendChild(privateMsg)
 })
 socket.on('myPrivate', (message) => {
-    console.log('private msg ', message)
-    const privateMsg = document.createElement('h3')
-    privateMsg.classList.add('private')
-    privateMsg.innerHTML = message
-    clients.appendChild(privateMsg)
+   console.log('private msg ', message)
+   const privateMsg = document.createElement('h3')
+   privateMsg.classList.add('private')
+   privateMsg.innerHTML = message
+   clients.appendChild(privateMsg)
 })
-
+ 
 const list = document.getElementById('messages')
 const chat = document.getElementById('chat')
 const input = document.getElementById('main-input')
@@ -106,56 +107,56 @@ const namespaceButton = document.getElementById('namespace-button')
 const adminButton = document.getElementById('admin-button')
 const idButton = document.getElementById('idButton')
 const memberList = document.getElementById('members')
-
+ 
 idButton.addEventListener('click', (e) => {
-    event.preventDefault()
-    const input = document.getElementById('id-string')
-    const id = input.value
-    const message = document.getElementById('solo-message')
-    const payload = message.value
-    console.log('payload ', payload)
-    socket.emit('get-id', ({id, payload, username}))
-    input.value = ''
-    message.value = ''
+   event.preventDefault()
+   const input = document.getElementById('id-string')
+   const id = input.value
+   const message = document.getElementById('solo-message')
+   const payload = message.value
+   console.log('payload ', payload)
+   socket.emit('get-id', ({id, payload, username}))
+   input.value = ''
+   message.value = ''
 })
 adminButton.addEventListener('click', (e) => {
-    console.log('socket ', socket.nsp)
-    if(socket.nsp === '/admin'){
-        socket.emit('adminButton', (username))
-    } else {
-        const no = document.createElement('h3')
-        no.innerHTML = 'Access Denied!'
-        no.classList.add('no-response')
-        clients.appendChild(no)
-    }
+   console.log('socket ', socket.nsp)
+   if(socket.nsp === '/admin'){
+       socket.emit('adminButton', (username))
+   } else {
+       const no = document.createElement('h3')
+       no.innerHTML = 'Access Denied!'
+       no.classList.add('no-response')
+       clients.appendChild(no)
+   }
 })
 namespaceButton.addEventListener('click', function(e) {
-    socket.emit('namespace', (username))
+   socket.emit('namespace', (username))
 })
 input.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      socket.emit('message', e.target.value, room)
-      e.target.value = ''
-    }
+   if (e.key === 'Enter') {
+     socket.emit('message', e.target.value, room)
+     e.target.value = ''
+   }
 });
-
+ 
 socket.once('meAdmin', (inforArr) => {
 const [ username, id ] = infoArr
-    console.log('ua', username, id)
-    const user = `${username} : ${id}`
-    const person = document.createElement('p')
-    person.innerHTML = user
-    memberList.appendChild(person)
-
+   console.log('ua', username, id)
+   const user = `${username} : ${id}`
+   const person = document.createElement('p')
+   person.innerHTML = user
+   memberList.appendChild(person)
+ 
 })
-
+ 
 socket.once('adminMember', (infoArr) => {
-    const [ username, id ] = infoArr
-    console.log('ua', username, id)
-    const user = `${username} : ${id}`
-    const person = document.createElement('p')
-    person.innerHTML = user
-    memberList.appendChild(person)
-
+   const [ username, id ] = infoArr
+   console.log('ua', username, id)
+   const user = `${username} : ${id}`
+   const person = document.createElement('p')
+   person.innerHTML = user
+   memberList.appendChild(person)
+ 
 })
 
